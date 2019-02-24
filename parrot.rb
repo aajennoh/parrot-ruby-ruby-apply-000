89b03c47fc97890ef1215f4ee712709640e203c5
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-24 08:52:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase="Pretty bird!")
-  phrase = "Squawk!"
   puts "#{phrase}"
+
+  phrase = "Squawk!"
   phrase
 
 end
